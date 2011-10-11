1218fb9f7d7b1f5cd4c553c56044eb58c8577daf
Jason Morrison <jmorrison@thoughtbot.com>
2011-10-10 20:05:54 -0400

    render the room list

--- app/assets/javascripts/backbone/views/room_list.js
+++ app/assets/javascripts/backbone/views/room_list.js
@@ -12,7 +12,17 @@ window.ChatApp.Views.RoomList = Backbone.View.extend({
 
   render: function() {
     $(this.el).empty().html(this.template({ rooms: this.collection }));
+    this.renderRooms();
     return this;
+  },
+
+  renderRooms: function() {
+    var ul = this.$("ul#chat-rooms");
+    var roomTemplate = JST['room_list_item'];
+
+    this.collection.each(function(room) {
+      ul.append(roomTemplate({ room: room }));
+    });
   },
 
   submit: function(event) {
--- spec/javascripts/views/room_list_spec.js
+++ spec/javascripts/views/room_list_spec.js
@@ -1,7 +1,13 @@
 describe("Views.RoomList", function() {
+  var rooms;
+
+  beforeEach(function() {
+    rooms = new ChatApp.Collections.Rooms([{ name: "Red" }, { name: "Blue" }]);
+  });
+
   it("renders a new room form", function() {
     var el = $("<div></div>");
-    var view = new ChatApp.Views.RoomList({ el: el });
+    var view = new ChatApp.Views.RoomList({ el: el, collection: rooms });
 
     view.render();
 
@@ -12,13 +18,27 @@ describe("Views.RoomList", function() {
 
   it("creates a new room when the form is submitted", function() {
     var el = $("<div></div>");
-    var roomsCollection = { create: sinon.stub() };
-    var view = new ChatApp.Views.RoomList({ el: el, collection: roomsCollection });
+    sinon.stub(rooms, "create");
+
+    var view = new ChatApp.Views.RoomList({ el: el, collection: rooms });
 
     view.render();
     $("input[name=name]", el).val("Watercooler");
     $("form", el).submit();
 
-    expect(roomsCollection.create).toHaveBeenCalled();
+    expect(rooms.create).toHaveBeenCalled();
+  });
+
+  it("renders the list of rooms", function() {
+    var el = $("<div></div>");
+    var view = new ChatApp.Views.RoomList({ el: el, collection: rooms });
+
+    view.render();
+
+    expect(el).toContain("ul#chat-rooms li a:contains('Red')")
+    expect(el).toContain("ul#chat-rooms li a:contains('Blue')")
+  });
+
+  it("re-renders when a model is added to the collection", function() {
   });
 });
